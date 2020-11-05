99739a5d82c508585b30bba5cbd13c258fde4dc7
Sebastian Malton <sebastian@malton.name>
2020-11-05 10:12:35 -0500

    Make Badge expandable if normal width is too narrow
    - Don't show click affordance if not hidding anything
    
    - Don't allow click to expand for flat badges
    
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/renderer/components/badge/badge.tsx
+++ src/renderer/components/badge/badge.tsx
@@ -1,25 +1,84 @@
 import "./badge.scss";
 
 import React from "react";
+import { computed, observable } from "mobx";
+import { observer } from "mobx-react";
 import { cssNames } from "../../utils/cssNames";
 import { TooltipDecoratorProps, withTooltip } from "../tooltip";
+import { autobind } from "../../utils";
 
 export interface BadgeProps extends React.HTMLAttributes<any>, TooltipDecoratorProps {
   small?: boolean;
   flat?: boolean;
   label?: React.ReactNode;
+  isExpanded?: boolean; // always force state to this value
 }
 
 @withTooltip
+@observer
 export class Badge extends React.Component<BadgeProps> {
-  render() {
-    const { className, label, small, flat, children, ...elemProps } = this.props;
+  @observable _isExpanded = false;
+  @observable hasHighlightedText = false;
+  @observable.ref elem?: HTMLElement;
 
-    return <>
-      <span className={cssNames("Badge", { small, flat }, className)} {...elemProps}>
+  componentWillUnmount() {
+    document.removeEventListener("selectionchange", this.onSelectionChange);
+  }
+
+  @computed get isExpanded() {
+    return this.props.isExpanded ?? this._isExpanded;
+  }
+
+  @computed get isExpandable() {
+    const { flat } = this.props;
+    const { scrollWidth, clientWidth, clientHeight, scrollHeight } = this.elem || {};
+
+    return !flat && (clientWidth < scrollWidth || clientHeight < scrollHeight);
+  }
+
+  @autobind()
+  setRef(elem: HTMLElement) {
+    // This needs to be a seperate function, see: https://github.com/facebook/react/issues/11258
+    this.elem = elem;
+  }
+
+  @autobind()
+  onSelectionChange() {
+    this.hasHighlightedText ||= document.getSelection().toString().length > 0;
+  }
+
+  @autobind()
+  onMouseDown() {
+    this.onSelectionChange(); // initial "event" fire on mouse down (for clearing old selections)
+    document.addEventListener("selectionchange", this.onSelectionChange);
+  }
+
+  @autobind()
+  onMouseUp() {
+    document.removeEventListener("selectionchange", this.onSelectionChange);
+
+    if (!this.hasHighlightedText) {
+      this._isExpanded = !this._isExpanded;
+    }
+
+    this.hasHighlightedText = false;
+  }
+
+  render() {
+    const { isExpandable, isExpanded } = this;
+    const { className, label, small, flat, children, isExpanded: _, ...elemProps } = this.props;
+    const classNames = cssNames("Badge", className, {
+      small,
+      flat,
+      isExpandable,
+      isExpanded,
+    });
+
+    return (
+      <div {...elemProps} className={classNames} onMouseUp={this.onMouseUp} onMouseDown={this.onMouseDown} ref={this.setRef}>
         {label}
         {children}
-      </span>
-    </>;
+      </div>
+    );
   }
 }
