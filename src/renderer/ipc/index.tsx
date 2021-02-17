4d222fcbbd6e450c506edf38f9011ba5e8a2e0c4
Sebastian Malton <sebastian@malton.name>
2021-02-17 12:00:38 -0500

    Add notification to user to add accessible namespaces
    - If cluster's refresh fails due to permissions
    
    - Broadcasting event and then receiving it in renderer
    
    - rename some IPC files, add target highlighting, add main side logging
    
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/renderer/ipc/index.tsx
+++ src/renderer/ipc/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { ipcRenderer, IpcRendererEvent } from "electron";
-import { areArgsUpdateAvailableFromMain, UpdateAvailableChannel, onCorrect, UpdateAvailableFromMain, BackchannelArg } from "../../common/ipc";
+import { areArgsUpdateAvailableFromMain, UpdateAvailableChannel, onCorrect, UpdateAvailableFromMain, BackchannelArg, ClusterListNamespaceForbiddenChannel, argArgsListNamespaceFordiddenArgs, ListNamespaceForbiddenArgs } from "../../common/ipc";
 import { Notifications, notificationsStore } from "../components/notifications";
 import { Button } from "../components/button";
 import { isMac } from "../../common/vars";
 import * as uuid from "uuid";
 import { invalidKubeconfigHandler } from "./invalid-kubeconfig-handler";
+import { clusterStore } from "../../common/cluster-store";
+import { navigate } from "../navigation";
+import { clusterSettingsURL } from "../components/+cluster-settings";
 
 function sendToBackchannel(backchannel: string, notificationId: string, data: BackchannelArg): void {
   notificationsStore.remove(notificationId);
@@ -43,11 +46,48 @@ function UpdateAvailableHandler(event: IpcRendererEvent, ...[backchannel, update
           <Button active outlined label="No" onClick={() => sendToBackchannel(backchannel, notificationId, { doUpdate: false })} />
         </div>
       </div>
-    ), {
+    ),
+    {
       id: notificationId,
       onClose() {
         sendToBackchannel(backchannel, notificationId, { doUpdate: false });
       }
+    }
+  );
+}
+
+const listNamespacesForbiddenHandlerDisplayedAt = new Map<string, number>();
+const intervalBetweenNotifications = 1000 * 60; // 60s
+
+function ListNamespacesForbiddenHandler(event: IpcRendererEvent, ...[clusterId]: ListNamespaceForbiddenArgs): void {
+  const lastDisplayedAt = listNamespacesForbiddenHandlerDisplayedAt.get(clusterId);
+  const wasDisplayed = Boolean(lastDisplayedAt);
+  const now = Date.now();
+
+  if (!wasDisplayed || (now - lastDisplayedAt) > intervalBetweenNotifications) {
+    listNamespacesForbiddenHandlerDisplayedAt.set(clusterId, now);
+  } else  {
+    // don't bother the user too often
+    return;
+  }
+
+  const notificationId = `list-namespaces-forbidden:${clusterId}`;
+
+  Notifications.info(
+    (
+      <div className="flex column gaps">
+        <b>Add Accessible Namespaces</b>
+        <p>Cluster <b>{clusterStore.active.name}</b> does not have permissions to list namespaces. Please add the namespaces you have access to.</p>
+        <div className="flex gaps row align-left box grow">
+          <Button active outlined label="Cluster Settings" onClick={()=> {
+            navigate(clusterSettingsURL({ params: { clusterId }, fragment: "accessible-namespaces" }));
+            notificationsStore.remove(notificationId);
+          }} />
+        </div>
+      </div>
+    ),
+    {
+      id: notificationId,
     }
   );
 }
@@ -60,4 +100,10 @@ export function registerIpcHandlers() {
     verifier: areArgsUpdateAvailableFromMain,
   });
   onCorrect(invalidKubeconfigHandler);
+  onCorrect({
+    source: ipcRenderer,
+    channel: ClusterListNamespaceForbiddenChannel,
+    listener: ListNamespacesForbiddenHandler,
+    verifier: argArgsListNamespaceFordiddenArgs,
+  });
 }
